ae2b1dbab026114902e1f8aa3f3fba9ad9ab952c
GabrielePeperna <gabrielepeperna98@gmail.com>
2022-11-30 11:07:31 +0100

    Creazione progetto Autonoleggio
    Progetto vuoto Autonoleggio

---
+++ src/test/java/com/agg/AutonoleggioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonoleggioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
